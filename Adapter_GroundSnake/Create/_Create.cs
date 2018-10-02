cef945c7e5b4425d76ba78a9e091203c16581438
Isak Naslund <Isak.Naslund@burohappold.com>
2018-10-02 14:09:12 +0200

    cleanup of old test method

--- Adapter_GroundSnake/Create/_Create.cs
+++ Adapter_GroundSnake/Create/_Create.cs
@@ -24,10 +24,7 @@ namespace BH.UI.GroundSnake.Adapter
         {
             if (objects.Count() > 0)
             {
-                if (objects.First() is Bar)
-                {
-                    CreateCollection(objects as IEnumerable<Bar>);
-                }
+               
             }
 
             return true;
@@ -37,42 +34,5 @@ namespace BH.UI.GroundSnake.Adapter
         /***************************************************/
 
 
-        private void CreateCollection(IEnumerable<Bar> enumerable)
-        {
-
-            // Get the current document and database
-            Document acDoc = Application.DocumentManager.MdiActiveDocument;
-            Database acCurDb = acDoc.Database;
-
-
-            // Start a transaction
-            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
-            {
-                // Open the Block table for read
-                BlockTable acBlkTbl;
-                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
-                                                OpenMode.ForRead) as BlockTable;
-
-                // Open the Block table record Model space for write
-                BlockTableRecord acBlkTblRec;
-                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
-                                                OpenMode.ForWrite) as BlockTableRecord;
-
-                // Create a line that starts at 5,5 and ends at 12,3
-                using (Line acLine = new Line(new Point3d(5, 5, 0),
-                                              new Point3d(12, 3, 0)))
-                {
-
-                    // Add the new object to the block table record and the transaction
-                    acBlkTblRec.AppendEntity(acLine);
-                    acTrans.AddNewlyCreatedDBObject(acLine, true);
-                }
-
-                // Save the new object to the database
-                acTrans.Commit();
-            }
-
-            
-        }
     }
 }
