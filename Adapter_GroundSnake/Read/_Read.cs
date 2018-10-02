e1a6082bd74a84cb257943910d77d51f0056f148
Isak Naslund <Isak.Naslund@burohappold.com>
2018-10-02 14:10:08 +0200

    More cleanup of test methods

--- Adapter_GroundSnake/Read/_Read.cs
+++ Adapter_GroundSnake/Read/_Read.cs
@@ -26,7 +26,7 @@ namespace BH.UI.GroundSnake.Adapter
     {
 
         /***************************************************/
-        /****           Adapter Methods                 ****/
+        /**** Adapter Methods                           ****/
         /***************************************************/
 
         protected override IEnumerable<IBHoMObject> Read(Type type, IList ids)
@@ -38,13 +38,13 @@ namespace BH.UI.GroundSnake.Adapter
             }
 
             return new List<IBHoMObject>();
-
-            //throw new NotImplementedException();
         }
 
         /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
 
-        public List<BHC.Pipe> ReadPipes(List<string> ids = null)
+        private List<BHC.Pipe> ReadPipes(List<string> ids = null)
         {
             CivilDocument doc = CivilApplication.ActiveDocument;
 
@@ -73,54 +73,6 @@ namespace BH.UI.GroundSnake.Adapter
 
 
         /***************************************************/
-
-        //    //Test method
-        //private List<Bar> ReadBars(List<string> ids = null)
-        //{
-        //    // Get the current document and database
-        //    Document acDoc = Application.DocumentManager.MdiActiveDocument;
-        //    Database acCurDb = acDoc.Database;
-
-        //    List<Bar> barList = new List<Bar>();
-
-        //    // Start a transaction
-        //    using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
-        //    {
-        //        // Open the Block table for read
-        //        BlockTable acBlkTbl;
-        //        acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
-        //                                     OpenMode.ForRead) as BlockTable;
-
-        //        // Open the Block table record Model space for read
-        //        BlockTableRecord acBlkTblRec;
-        //        acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
-        //                                        OpenMode.ForRead) as BlockTableRecord;
-
-        //        // Step through the Block table record
-        //        foreach (ObjectId asObjId in acBlkTblRec)
-        //        {
-        //            if (asObjId.ObjectClass.DxfName == "LINE")
-        //            {
-        //                Line line = acTrans.GetObject(asObjId, OpenMode.ForRead) as Line;
-        //                Bar bar = new Bar
-        //                {
-        //                    StartNode = new Node { Position = new oM.Geometry.Point { X = line.StartPoint.X, Y = line.StartPoint.Y, Z = line.StartPoint.Z } },
-        //                    EndNode = new Node { Position = new oM.Geometry.Point { X = line.EndPoint.X, Y = line.EndPoint.Y, Z = line.EndPoint.Z } }
-        //                };
-
-        //                barList.Add(bar);
-        //            }
-        //            //acDoc.Editor.WriteMessage("\nDXF name: " + asObjId.ObjectClass.DxfName);
-        //            //acDoc.Editor.WriteMessage("\nObjectID: " + asObjId.ToString());
-        //            //acDoc.Editor.WriteMessage("\nHandle: " + asObjId.Handle.ToString());
-        //            //acDoc.Editor.WriteMessage("\n");
-        //        }
-
-        //        acTrans.Commit();
-                
-        //        // Dispose of the transaction
-        //    }
-        //    return barList;
-        //}
+        
     }
 }
