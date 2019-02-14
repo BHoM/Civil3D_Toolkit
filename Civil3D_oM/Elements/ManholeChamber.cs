f833b9f1be387fed4dbba0e7accb7b1edd76fb5b
Conor Hubert <Conor.Hubert@BuroHappold.com>
2019-02-14 16:14:48 +0000

    Add ManholeChamber
    Added ManholeChamber object with initial properties.

---
+++ Civil3D_oM/Elements/ManholeChamber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+namespace BH.oM.Civils.Elements
+{
+    public class ManholeChamber : BHoMObject
+    {
+        /***************************************************/
+        /**** Public Properties                         ****/
+        /***************************************************/
+
+        public double CentrePoint { get; set; } = 0;
+
+        public double InternalLength { get; set; } = 0;
+
+        public double InternalWidth { get; set; } = 0;
+
+        public double InternalDepth { get; set; } = 0;
+
+        public double WallThickness { get; set; } = 0;
+
+        public double SurroundThickness { get; set; } = 0;
+
+        public double ChamberOrientation { get; set; } = 0;
+
+        public ChamberShape ChamberShape { get; set; } = ChamberShape.Undefined;
+
+        public double CoverLength { get; set; } = 0;
+
+        public double CoverWidth { get; set; } = 0;
+
+        public double CoverDepth { get; set; } = 0;
+
+        public double CoverOrientation { get; set; } = 0;
+
+        public double BeddingDepth { get; set; } = 0;
+
+        /***************************************************/
+    }
+}
