9f6842b387c29f8bd26ca68577a3189f709a43b9
Conor Hubert <Conor.Hubert@BuroHappold.com>
2019-03-18 16:35:48 +0000

    Initial commit of surface pulling

---
+++ Civil3D_oM/Elements/Surface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+namespace BH.oM.Civils.Elements
+{
+    public class Surface : BHoMObject
+    {
+        /***************************************************/
+        /**** Public Properties                         ****/
+        /***************************************************/
+
+        public List<Polyline> Triangles { get; set; } = new List<Polyline>();
+
+        public double MaximumGradeOrSlope { get; set; } = 0;
+
+        public double MeanGradeOrSlope { get; set; } = 0;
+
+        public double MinimumGradeOrSlope { get; set; } = 0;
+
+        public double SurfaceArea2D { get; set; } = 0;
+
+        public double SurfaceArea3D { get; set; } = 0;
+
+        /***************************************************/
+    }
+}
---
+++ Engine_GroundSnake/Convert/ToBHoM/Surface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHG = BH.oM.Geometry;
+using BHC = BH.oM.Civils.Elements;
+using ADC = Autodesk.Civil.DatabaseServices;
+
+namespace BH.Engine.GroundSnake
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static BHC.Surface ToBHoM(this ADC.TinSurface acTinSurface)
+        {
+            List<BHG.Polyline> polylines = new List<oM.Geometry.Polyline>();
+
+            foreach (ADC.TinSurfaceTriangle triangle in acTinSurface.Triangles)
+            {
+
+                List<BHG.Point> points = new List<BHG.Point> { ToBHoM(triangle.Vertex1.Location),
+                                                               ToBHoM(triangle.Vertex2.Location),
+                                                               ToBHoM(triangle.Vertex3.Location)};
+
+                polylines.Add(Geometry.Create.Polyline(points));
+            }
+
+            return new BHC.Surface
+            {
+                Triangles = polylines
+            };
+        }
+    }
+}
