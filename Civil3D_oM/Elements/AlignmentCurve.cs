4cdb2f4729ccc60bbcb24e6c6033779bd9b7f465
Conor Hubert <Conor.Hubert@BuroHappold.com>
2020-01-08 15:39:56 +0000

    AlignmentCurve object added.

---
+++ Civil3D_oM/Elements/AlignmentCurve.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+namespace BH.oM.Civils.Elements
+{
+    public class AlignmentCurve : BHoMObject
+    {
+        /***************************************************/
+        /**** Public Properties                         ****/
+        /***************************************************/
+
+        public ICurve Curve { get; set; } = new Polyline();
+
+        /***************************************************/
+    }
+}
