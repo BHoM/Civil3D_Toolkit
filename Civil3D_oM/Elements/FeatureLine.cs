7a9940629ed2d9ac4853f9cc8d3df13ba5d70c63
Maja Lindroth <maja.lindroth@burohappold.com>
2020-01-08 12:27:37 +0000

    #43 Create FeatureLine object

---
+++ Civil3D_oM/Elements/FeatureLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+
+namespace BH.oM.Civils.Elements
+{
+    /***************************************************/
+    /**** Public Properties                         ****/
+    /***************************************************/
+    public class FeatureLine : BHoMObject
+    {
+        public string Description { get; set; } = "";
+        public double Length2D { get; set; } = 0;
+        public double Length3D { get; set; } = 0;
+        public double MaxElevation { get; set; } = 0;
+        public double MaxGrade { get; set; } = 0;
+        public double MinElevation { get; set; } = 0;
+        public double MinGrade { get; set; } = 0;
+        public string FeatureLineName { get; set; } = "";
+        public double SiteID { get; set; } = 0;
+
+        /***************************************************/
+    }
+}
