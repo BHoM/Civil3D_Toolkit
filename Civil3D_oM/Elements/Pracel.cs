e5d8ee62798b0fba0db6e6136329d92f4d2d7e3e
Maja Lindroth <maja.lindroth@burohappold.com>
2020-01-08 11:57:48 +0000

    Create Parcel object

---
+++ Civil3D_oM/Elements/Pracel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Geometry;
+
+namespace BH.oM.Civils.Elements
+{
+    /***************************************************/
+    /**** Public Properties                         ****/
+    /***************************************************/
+    public class Pracel : BHoMObject
+    {
+        public Point AreaLocation { get; set; } = new Point();
+        public Point Centroid { get; set; } = new Point();
+        public string Desctiption { get; set; } = "";
+        public string ParcelName { get; set; } = "";
+        public int Number { get; set; } = 0;
+
+        /***************************************************/
+    }
+}
