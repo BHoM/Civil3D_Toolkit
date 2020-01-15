431f46a607d94aa36c6095e818c3ea935b459d64
Maja Lindroth <maja.lindroth@burohappold.com>
2020-01-15 12:37:02 +0000

    #43 Block object

---
+++ Civil3D_oM/Enums/CollisionType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.oM.Adapters.Civil3D
+{
+    public enum PackageType
+    {
+        Push,
+        Pull,
+        ConnectionCheck,
+        UpdateProperty,
+    }
+
+}
