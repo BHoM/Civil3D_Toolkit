9cf63e6d79ce8c1cc878e549acb548ee79f3077f
Maja Lindroth <maja.lindroth@burohappold.com>
2020-01-15 11:46:38 +0000

    #43 Alignment arc

--- Engine_GroundSnake/Convert/Geomerty/AlignmentCurve.cs
+++ Engine_GroundSnake/Convert/Geomerty/AlignmentCurve.cs
@@ -38,43 +38,11 @@ namespace BH.UI.Civil.Engine
 
         public static BHC.AlignmentCurve FromCivil3D(ADC.AlignmentArc aArc)
         {
-            /* List<Point> controlpoints = new List<Point>();
-             controlpoints.Add(aArc.StartPoint.FromCivil3D());
-             controlpoints.Add(aArc.CenterPoint.FromCivil3D());
-             controlpoints.Add(aArc.EndPoint.FromCivil3D()); */
-
-            /*Point origin = new BHG.Point { X = 0, Y = 0, Z = 0 };
-            Point axis = new BHG.Point { X = 10, Y = 0, Z = 0 };
-
-            // Vector v = new BHG.Vector { X = aArc.CenterPoint.FromCivil3D().X, Y = aArc.CenterPoint.FromCivil3D().Y, Z = aArc.CenterPoint.FromCivil3D().Z };
-
-            //Cartesian system = Create.CartesianCoordinateSystem(aArc.CenterPoint.FromCivil3D(),v , v);
-            BH.oM.Geometry.CoordinateSystem.Cartesian cs = BH.Engine.Geometry.Create.CartesianCoordinateSystem(aArc.CenterPoint.FromCivil3D(), v, v);
-
-            double startAngle = BH.Engine.Geometry.Query.Angle(aArc.StartPoint.FromCivil3D(), origin, axis);
-            double endAngle = BH.Engine.Geometry.Query.Angle(aArc.EndPoint.FromCivil3D(), origin, axis);
-            double radius = aArc.CenterPoint.FromCivil3D().Distance(aArc.StartPoint.FromCivil3D());
-
-            double t1 =  aArc.StartDirection;
-            double t2 = aArc.EndDirection;
-
-           // BHG.Arc arc = new Arc { CoordinateSystem = cs, Radius = radius, StartAngle = t2, EndAngle = t1 };
-            
-
-            //BHG.Polyline pl = new Polyline { ControlPoints = controlpoints };
-
             return new BHC.AlignmentCurve
             {
-                Curve = arc,
-            };*/
-
-            Arc a = BH.Engine.Geometry.Create.Arc(aArc.StartPoint.FromCivil3D(), aArc.PassThroughPoint2.FromCivil3D(), aArc.EndPoint.FromCivil3D());
-            return new BHC.AlignmentCurve
-            {
-                Curve = a,
+                Curve = BH.Engine.Geometry.Create.ArcByCentre(aArc.CenterPoint.FromCivil3D(), aArc.StartPoint.FromCivil3D(), aArc.EndPoint.FromCivil3D()),
             };
         }
-
 
         public static BHC.AlignmentCurve FromCivil3D(object o)
         {
